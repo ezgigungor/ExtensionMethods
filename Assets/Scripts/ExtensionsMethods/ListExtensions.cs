2bac8f3e95b235137bdab51191e63230daee23da
Ezgi Güngör <ezgigungor114@gmail.com>
2021-12-22 14:16:50 +0300

    add shuffle list extention method

--- Assets/Scripts/ExtensionsMethods/ListExtensions.cs
+++ Assets/Scripts/ExtensionsMethods/ListExtensions.cs
@@ -1,9 +1,17 @@
-using System.Collections;
 using System.Collections.Generic;
 using Random = UnityEngine.Random;
 
 public static class ListExtensions
 {
     public static T GetRandomItem<T>(this IList<T> list) => list[Random.Range(0, list.Count)];
-    
+    public static void Shuffle<T>(this IList<T> list)
+    {
+        for (int i = list.Count - 1; i > 1; i--)
+        {
+            int j = Random.Range(0, i + 1);
+            var element = list[j];
+            list[j] = list[i];
+            list[i] = element;
+        }
+    }
 }
--- Assets/Scripts/ExtensionsMethods/RendererExtensions.cs
+++ Assets/Scripts/ExtensionsMethods/RendererExtensions.cs
@@ -1,18 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class RendererExtensions : MonoBehaviour
+public static  class RendererExtensions
 {
-    // Start is called before the first frame update
-    void Start()
+    public static void SetAlpha(this SpriteRenderer renderer, float alpha)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        var color = renderer.color;
+        color.a = alpha;
+        renderer.color = color;
     }
 }
